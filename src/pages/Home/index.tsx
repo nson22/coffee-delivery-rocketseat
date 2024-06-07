530b585ab646df742051e15456703e4f19aaea61
Nilson Silva <glecinilson@gmail.com>
2024-06-07 13:40:31 -0400

    Estilizando a home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,64 @@
-import { ShoppingCart } from "@phosphor-icons/react";
+import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
 import ImageMakert from "../../assets/cd-image-market.svg";
-import { HomeContainer } from "./styles";
+import CafeExpresso from "../../assets/cd-expresso.svg";
+import { IntroMainContainer, IntroItems, IntroItemsGrid, IntroShoppingCartIcon, IntroSubtitle, IntroTitle, CoffeeCard, CoffeeCardImage, CoffeeCardTag, CoffeeCardContainer } from "./styles";
 
 export function Home() {
   return (
     <>
-      <HomeContainer>
-        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-        <p>
-          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-          hora
-        </p>
-        <span>
-          <i>
-            <ShoppingCart />
-          </i>
-          Compra simples e segura
-        </span>
-      </HomeContainer>
-      <img src={ImageMakert} />
+      <main>
+        <IntroMainContainer>
+          <div>
+            <IntroTitle>Encontre o café perfeito para qualquer hora do dia</IntroTitle>
+            <IntroSubtitle>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</IntroSubtitle>
+            <IntroItemsGrid>
+              <IntroItems>
+                <IntroShoppingCartIcon>
+                  <ShoppingCart weight="fill" />
+                </IntroShoppingCartIcon>
+                Compra simples e segura
+              </IntroItems>
+              <IntroItems>
+                <IntroShoppingCartIcon>
+                  <Package weight="fill" />
+                </IntroShoppingCartIcon>
+                Embalagem mantém o café intacto
+              </IntroItems>
+              <IntroItems>
+                <IntroShoppingCartIcon>
+                  <Timer weight="fill" />
+                </IntroShoppingCartIcon>
+                Entrega rápida e rastreada
+              </IntroItems>
+              <IntroItems>
+                <IntroShoppingCartIcon>
+                  <Coffee weight="fill" />
+                </IntroShoppingCartIcon>
+                O café chega fresquinho até você
+              </IntroItems>
+            </IntroItemsGrid>
+          </div>
+          <div>
+            <img src={ImageMakert} alt="" />
+          </div>
+        </IntroMainContainer>
+        <h1>Nossos Cafés</h1>
+        <CoffeeCard>
+          <CoffeeCardImage>
+            <img src={CafeExpresso} alt="" />
+          </CoffeeCardImage>
+          <CoffeeCardContainer>
+            <CoffeeCardTag>TRADICIONAL</CoffeeCardTag>
+            <h1>Expresso Tradicional</h1>
+            <span>O tradicional café feito com água quente e grãos moídos</span>
+            <span>R$ 9,90</span>
+            <button type="button">-</button>
+            <span>99</span>
+            <button type="button">+</button>
+            <button type="button"><ShoppingCart weight="fill" /></button>
+          </CoffeeCardContainer>
+        </CoffeeCard>
+      </main>
     </>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,36 +1,90 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.div`
+export const IntroMainContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  height: 544px;
+  justify-content: space-between;
 `;
 
-export const TitleContainer = styled.h1`
+export const IntroTitle = styled.h1`
   font-family: "Baloo 2", sans-serif;
   color: ${(props) => props.theme["title"]};
   font-weight: bold;
+  font-size: 3rem;
+  width: 588px;
+  line-height: 1.6;
 `;
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+export const IntroSubtitle = styled.p`
+  font-size: 1.25rem;
+  height: 52px;
+  margin-top: 1rem;
+`
 
-export const GridContainer = styled.div`
-  margin-top: 2.5rem;
-  align-self: start;
+export const IntroItemsGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
+  margin-top: 2.5rem;
+  align-self: start;
   gap: 1.25rem;
-`;
+  column-gap: 2.5rem;
+`
 
-export const IconContainer = styled.i`
+export const IntroIconBase = styled.i`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 2rem;
   width: 2rem;
-  background: ${(props) => props.theme["yellow-dark"]};
   border-radius: 100%;
+`;
+
+
+export const IntroShoppingCartIcon = styled((IntroIconBase))`
+  background: ${(props) => props.theme["yellow-dark"]};
   color: ${(props) => props.theme["background"]};
 `;
+
+
+export const IntroItems = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+`
+
+export const IntroImage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 29.75rem;
+  height: 22.5rem;
+`
+
+
+export const CoffeeCard = styled.div`
+  height: 19.375rem;
+  width: 16rem;
+  border-radius: 6px 36px;
+  background: ${props => props.theme["base-card"]};
+  position: relative;
+`
+
+export const CoffeeCardImage = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  `
+export const CoffeeCardContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+`
+
+export const CoffeeCardTag = styled.span`
+  border-radius: 100px;
+  padding:  0.25rem 0.5rem;
+  color: ${props => props.theme["yellow-dark"]};
+  background: ${props => props.theme["yellow-light"]};
+`
